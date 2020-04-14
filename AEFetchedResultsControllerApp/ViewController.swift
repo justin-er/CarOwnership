97ef5e744b7963e08e520b839a5120901eead62a
Amirreza Eghtedari <eghtedari.amir@gmail.com>
2020-04-14 18:42:32 +0430

    Initial Commit

---
+++ AEFetchedResultsControllerApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AEFetchedResultsControllerApp
+//
+//  Created by Amirreza Eghtedari on 14.04.20.
+//  Copyright © 2020 Amirreza Eghtedari. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
